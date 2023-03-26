efa404b524b6f7680cae0846fe4ce27c3132f174
Mirko Contini <mirko.contini01@gmail.com>
2023-03-26 17:32:37 +0200

    Ricominciato da 0 lo script del player
    Ora il movimento del player viene gestito tramite un characterController invece che co il rigidbody

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,92 +5,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody rb;
-    public Transform cam;
-
-    private float mass = 1;
-    private float drag = 2;
-    public float speed;
-    public float jumpSpeed;
-    private bool isGrounded = false;
-    private int jumpCount;
-
+    CharacterController ch;
+    private float movementSpeed = 5.0f;
+    private float jumpSpeed = 7.0f;
+    private const float gravity = 9.81f;
+    float vspeed = 0;
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        rb.mass = mass;
-        rb.drag = drag;
-        speed = 0.5f;
-        jumpSpeed = 0;
-        jumpCount = 0;
-
+        ch = GetComponent<CharacterController>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        Debug.Log("diocane");
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
 
-        Vector3 camForward = cam.forward;
-        Vector3 camRight = cam.right;
+        Vector3 movement = new Vector3(horizontalMovement, 0f, verticalMovement)*movementSpeed;
 
-        camForward.y = 0;
-        camRight.y = 0;
-
-        Vector3 forwardRelative = verticalInput * camForward;
-        Vector3 rightRelative = horizontalInput * camRight;
-
-        Vector3 movementDirection = forwardRelative + rightRelative;
-
-        Vector3 movement = new Vector3(movementDirection.x * speed, 0f, movementDirection.z * speed);
-
-
-        if (Input.GetKeyDown("space") && isGrounded)
+        if (ch.isGrounded)
         {
-            switch (jumpCount)
-            {
-                case 0:
-                    jumpSpeed = 3.0f;
-                    rb.AddForce(new Vector3(0f, jumpSpeed, 0f), ForceMode.Impulse);
-                    jumpCount++;
-                    Debug.Log("Primo");
-                    break;
-
-                case 1:
-                    jumpSpeed = 1.0f;
-                    rb.AddForce(new Vector3(0f, jumpSpeed, 0f), ForceMode.Impulse);
-                    jumpCount++;
-                    Debug.Log("Secondo");
-                    break;
-
-                case 2:
-                    isGrounded = false;
-                    break;
-            }
+            float jump = Input.GetAxis("Jump") * jumpSpeed;
+            vspeed = jump;
         }
 
-        if (isGrounded)
-        {
-            rb.AddForce(movement, ForceMode.Impulse);
-        }
+        vspeed -= gravity*Time.deltaTime;
+        movement.y = vspeed;
+        ch.Move(movement * Time.deltaTime);
     }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.collider.CompareTag("Floor"))
-        {
-            isGrounded = true;
-            if (jumpCount != 0)
-                jumpCount = 0;
-        }
-    }
-
-   /* private void OnCollisionExit(Collision collision)
-    {
-        if (collision.collider.CompareTag("Floor"))
-            isGrounded = false;
-        
-    }*/
 }
 
