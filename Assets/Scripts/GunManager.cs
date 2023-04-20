efdbd0f64eb50119a7a90e135fd5c76c119c1f0d
Mirko Contini <mirko.contini01@gmail.com>
2023-04-20 16:38:02 +0200

    Implementati metodi aim() e shoot()
    Implementati metodi per mirare e sparare tenendo conto del rateo di fuoco delle singole armi

--- Assets/Scripts/GunManager.cs
+++ Assets/Scripts/GunManager.cs
@@ -1,33 +1,41 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GunManager : MonoBehaviour
 {
+    RaycastHit hitPoint;
+    public Camera playerCamera;
     /*Creo delle variabili che contengono i valori da passare al costruttore della classe weapon quando 
       istanzierò gli oggetti per ogni arma*/
-    float gunFireRate; //Contiene il tempo che deve passare tra uno sparo e l'altro
-    Vector3 gunIdlePosition=new Vector3 (1f,-0.6f,0.7f);
-    Vector3 gunAimPosition=new Vector3 (-2f, 0.75f,-4f);
+    float gunFireRate =0.5f; //Contiene il tempo che deve passare tra uno sparo e l'altro.
+    float gunDamage=10.0f;
+    Vector3 gunIdlePosition=new Vector3 (1f,-0.4f,0.5f);
+    Vector3 gunAimPosition=new Vector3 (0f, -0.25f,1f);
     Weapon gun;
 
-    float machineGunFireRate; //Contiene il tempo che deve passare tra uno sparo e l'altro
-    Vector3 machineGunIdlePosition=new Vector3 (0.5f,-0.2f,0.2f);
-    Vector3 machineGunAimPosition=new Vector3 (-2f, 0.75f,-4f);
+    float machineGunFireRate=0.1f; //Contiene il tempo che deve passare tra uno sparo e l'altro
+    float machineGunDamage=7.0f;
+    Vector3 machineGunIdlePosition=new Vector3 (1f,-0.4f,0.5f);
+    Vector3 machineGunAimPosition=new Vector3 (0f, -0.5f,0.8f);
     Weapon machineGun;
 
-    float shotGunFireRate; //Contiene il tempo che deve passare tra uno sparo e l'altro
-    Vector3 shotGunIdlePosition=new Vector3 (1f,-0.6f,0.7f);
-    Vector3 shotGunAimPosition=new Vector3 (-2f, 0.75f,-4f);
+    float shotGunFireRate=1.5f; //Contiene il tempo che deve passare tra uno sparo e l'altro
+    Vector3 shotGunIdlePosition=new Vector3 (0.3f,-0.2f,0.5f);
+    Vector3 shotGunAimPosition=new Vector3 (0f, -0.2f,0.3f);
+    float shotGunDamage=50.0f;
     Weapon shotGun;
 
     List<Weapon> weaponList= new List<Weapon>(); //Lista di tutte le armi impugnate dal player
+
+    float timeSinceLastShoot=0; //Contiene il tempo trascorso dall'ulitmo sparo
+
     int selectedWeapon; //Variabile che contiene l'indice dell'arma selezionata in quel momento
     private void Start() {
         /*Creazione delle singole armi*/
-        gun=new Weapon (gunIdlePosition,gunAimPosition,gunFireRate,20);
-        machineGun=new Weapon(machineGunIdlePosition, machineGunAimPosition,machineGunFireRate,10);
-        shotGun=new Weapon (shotGunIdlePosition,shotGunAimPosition,shotGunFireRate,20);
+        gun=new Weapon (gunIdlePosition,gunAimPosition,gunFireRate,gunDamage,false);
+        machineGun=new Weapon(machineGunIdlePosition, machineGunAimPosition,machineGunFireRate,machineGunDamage,true);
+        shotGun=new Weapon (shotGunIdlePosition,shotGunAimPosition,shotGunFireRate,shotGunDamage,false);
 
         /*Aggiunta delle armi alla lista*/
         weaponList.Add(gun);
@@ -35,11 +43,24 @@ public class GunManager : MonoBehaviour
         weaponList.Add(shotGun);
         selectedWeapon=0;
         selectWeapon(); //Di default viene selezionata la prima arma della lista
+
     }
 
     private void Update() {
+        timeSinceLastShoot+=Time.deltaTime;
+        
         weaponSwitch();
+        aim(weaponList[selectedWeapon],playerCamera);
+
+        if(Input.GetKey(KeyCode.Mouse0) && timeSinceLastShoot>=weaponList[selectedWeapon].fireRate){
+            shoot(weaponList[selectedWeapon],playerCamera);
+            timeSinceLastShoot=0;
+        }
+        
     }
+
+
+
 
     /*Metodo che seleziona l'arma corretta*/
     void selectWeapon(){
@@ -63,7 +84,8 @@ public class GunManager : MonoBehaviour
 
     /*Metodo che switcha le armi tramite lo scroll della rotella del mouse*/
     void weaponSwitch(){
-        int previousSelectedWeapon=selectedWeapon;
+        int previousSelectedWeapon=selectedWeapon; //Salvo in una variabile l'arma precedentemente attiva
+
         if(Input.GetAxis("Mouse ScrollWheel") >=0f){
             if(selectedWeapon>=transform.childCount-1)
                 selectedWeapon=0;
@@ -77,7 +99,34 @@ public class GunManager : MonoBehaviour
             else 
                 selectedWeapon--;
         }
-        if(selectedWeapon!=previousSelectedWeapon)
+
+        //Se l'arma attuale è diversa da quella precedente, allora faccio lo switch
+        if(selectedWeapon!=previousSelectedWeapon){ 
             selectWeapon();
+        }
+            
+    }
+
+   void aim(Weapon activeWeapon, Camera playerCamera){
+        if(Input.GetKeyDown(KeyCode.Mouse1) && !activeWeapon.isAiming){
+            activeWeapon.isAiming=true;
+            transform.localPosition=activeWeapon.aimPosition;
+            playerCamera.fieldOfView=50;
+        }
+
+        if(Input.GetKeyUp(KeyCode.Mouse1) && activeWeapon.isAiming){
+            activeWeapon.isAiming=false;
+            transform.localPosition=activeWeapon.idlePosition;
+            playerCamera.fieldOfView=80;
+        }
+    }
+
+   void shoot(Weapon activeWeapon, Camera playerCamera){
+        RaycastHit hitPoint;
+        bool rayCastRes= Physics.Raycast(playerCamera.transform.position,playerCamera.transform.forward,out hitPoint, Mathf.Infinity);
+        if(rayCastRes ){
+            print(hitPoint.transform.tag);
+            print(activeWeapon.damage);
+        }  
     }
 }
