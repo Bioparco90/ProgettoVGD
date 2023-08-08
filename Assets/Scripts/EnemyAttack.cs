74f1ab42fa257cdb99beac0a6e058a1c5a589713
Mirko Contini <mirko.contini01@gmail.com>
2023-08-08 16:46:09 +0200

    .

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Diagnostics;
-
 public class EnemyAttack : MonoBehaviour
 {
     public Transform hitPoint;
@@ -11,19 +9,11 @@ public class EnemyAttack : MonoBehaviour
 
     void Attack()
     {
-        StackTrace stackTrace = new StackTrace();           // get call stack
-        StackFrame[] stackFrames = stackTrace.GetFrames();  // get method calls (frames)
-
-        // write call stack method names
-        foreach (StackFrame stackFrame in stackFrames)
-        {
-            print(stackFrame.GetMethod().CallingConvention);   // write method name
-        }
         Collider[] hitPlayer = Physics.OverlapSphere(hitPoint.position, attackRange, playerLayer);
-        //print(hitPlayer.Length);
+        print(hitPlayer.Length);
         foreach (Collider player in hitPlayer)
         {
-            //print("Colpito " + player.name);
+            print("Colpito " + player.name);
             player.GetComponent<PlayerController>().takeDamage(10);
         }
     }
