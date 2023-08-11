e1c1e32da836b546d906c08dc7ead98d5e86faf3
Bioparco <monnimarco90@gmail.com>
2023-08-11 18:50:27 +0200

    Select level from main menu done

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -10,6 +10,11 @@ public class MainMenuScript : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
+    public void SelectLevel(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
     public void QuitGame()
     {
         Application.Quit();
