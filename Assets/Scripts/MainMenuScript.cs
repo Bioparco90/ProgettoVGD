f1223519d1dcffd329e75ccc63f06058e68f9223
Bioparco <monnimarco90@gmail.com>
2023-08-20 20:23:36 +0200

    Refactoring menu

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -1,11 +1,9 @@
 using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.EventSystems;
 
 public class MainMenuScript : MonoBehaviour
 {
-    public GunManager gunManager;
 
     public void PlayGame()
     {
@@ -22,37 +20,8 @@ public class MainMenuScript : MonoBehaviour
         Application.Quit();
     }
 
-    public void Resume()
-    {
-        gunManager.TogglePause();
-    }
-
-    public void SaveGame()
+    public static void LoadGame()
     {
         throw new NotImplementedException();
-    }
-
-    public void LoadGame()
-    {
-        throw new NotImplementedException();
-    }
-
-    public void BackToStart()
-    {
-        SceneManager.LoadScene("Menu");
-    }
-
-
-    // Le due funzioni seguenti gestiscono il reset del focus dei bottoni (rimanevano selezionati anche dopo l'uscita dal menu)
-    private void OnEnable()
-    {
-        // Quando il menu di pausa viene attivato, reimposta lo stato dei bottoni
-        EventSystem.current.SetSelectedGameObject(null); // Rimuovi il focus da qualsiasi oggetto
-    }
-
-    private void OnDisable()
-    {
-        // Quando il menu di pausa viene disattivato, reimposta lo stato dei bottoni
-        EventSystem.current.SetSelectedGameObject(null); // Rimuovi il focus da qualsiasi oggetto
     }
 }
